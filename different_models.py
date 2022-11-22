ce4e37584d5af6779e511b32a61790e2aa9b7a61
Yogel9 <Dovidenkov@users.noreply.github.com>
2022-11-22 17:41:46 +0300

    Вынесли все модели в отдельный файл

---
+++ different_models.py
@@ -0,0 +1,107 @@
+from matplotlib import pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import SGDRegressor
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import Ridge, Lasso
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from tabulate import tabulate
+from prettytable import PrettyTable
+
+
+def print_MSE_MAE(y_train, y_test, y_train_prediction, y_test_prediction, title=None):
+    """вычисляем средневкадратичные и абсолютные ошибки"""
+    Train_MSE = mean_squared_error(y_train, y_train_prediction)
+    Test_MSE = mean_squared_error(y_test, y_test_prediction)
+    Train_MAE = mean_absolute_error(y_train, y_train_prediction)
+    Test_MAE = mean_absolute_error(y_test, y_test_prediction)
+
+    table = PrettyTable()
+    table.title = title
+    table.field_names = ['Train MSE: ', 'Test MSE: ', 'Train MAE: ', 'Test MAE: ']
+    table.add_row([Train_MSE, Test_MSE, Train_MAE, Test_MAE])
+    print(table.get_string())
+    # print('Train MSE: ', Train_MSE)  # чувствителен к выбросам в выборке
+    # print('Test MSE: ', Test_MSE)
+    #
+    # print('Train MAE: ', Train_MAE)  # усреднённая сумма модулей разницы между реальным и предсказанным значениями
+    # print('Test MAE: ', Test_MAE)
+    return Train_MSE, Test_MSE, Train_MAE, Test_MAE
+
+
+def Linear_Regression(data_dict):
+    """МНК"""""
+    model = LinearRegression()
+    model.fit(data_dict['train'][0], data_dict['train'][1])
+
+    y_train_prediction = model.predict(data_dict['train'][0])
+    y_test_prediction = model.predict(data_dict['test'][0])
+
+    print("LinearRegression")
+    print_MSE_MAE(data_dict['train'][1], data_dict['test'][1], y_train_prediction, y_test_prediction,"Linear_Regression")
+    return model.coef_
+
+
+def Gradient_Linear_Regression(data_dict):
+    """Градиентный спуск"""
+    gradient_model = SGDRegressor(tol=.0001, eta0=.01)  # прекращение итерации и скорость обучения
+    gradient_model.fit(data_dict['train'][0], data_dict['train'][1])
+
+    y_train_prediction = gradient_model.predict(data_dict['train'][0])
+    y_test_prediction = gradient_model.predict(data_dict['test'][0])
+
+    print("SGDLinearRegression")
+    print_MSE_MAE(data_dict['train'][1], data_dict['test'][1], y_train_prediction, y_test_prediction, "Gradient_Linear_Regression")
+    return gradient_model.coef_
+
+
+def Polynomial_Linear_Regression(data_dict):
+    """линейная регресси с полиномиальные показателями"""
+    poly = PolynomialFeatures(2, include_bias=False)  # степень, исключаем x[0]**2
+    poly_df = poly.fit_transform(data_dict['train'][0])
+    # нормировка данных
+    scaled_poly_df = scaler.fit_transform(poly_df)
+    print(f" Количество показателей было: {data_dict['train'][0].shape}")
+    print(f" Количество показателей стало: {scaled_poly_df.shape}")
+    gradient_model.fit(scaled_poly_df, data_dict['train'][1])
+
+    y_train_prediction = gradient_model.predict(data_dict['train'][0])
+    y_test_prediction = gradient_model.predict(data_dict['test'][0])
+    print("LinearRegression+PolynomialFeatures")
+    print_MSE_MAE(data_dict['train'][1], data_dict['test'][1], y_train_prediction, y_test_prediction)
+
+
+def Ridge_Linear_Regression(data_dict):
+    """Ridge Regression Model"""
+    ridgeReg = Ridge(alpha=10)
+
+    ridgeReg.fit(data_dict['train'][0], data_dict['train'][1])
+    y_train_prediction = ridgeReg.predict(data_dict['train'][0])
+    y_test_prediction = ridgeReg.predict(data_dict['test'][0])
+    print("Ridge")
+    print_MSE_MAE(data_dict['train'][1], data_dict['test'][1], y_train_prediction, y_test_prediction)
+    return ridgeReg.coef_
+
+
+def Lasso_Linear_Regression(data_dict):
+    """Lasso Regression Model"""
+    lasso = Lasso(alpha=1)
+    lasso.fit(data_dict['train'][0], data_dict['train'][1])
+    y_train_prediction = lasso.predict(data_dict['train'][0])
+    y_test_prediction = lasso.predict(data_dict['test'][0])
+    print("Laso")
+    print_MSE_MAE(data_dict['train'][1], data_dict['test'][1], y_train_prediction, y_test_prediction)
+    return lasso.coef_
+
+
+def show_weights(columns_name, coef):
+    """Визуализируем получившиеся веса"""
+    table = PrettyTable()
+    table.title = "Веса"
+    table.field_names = columns_name
+    table.add_row(coef)
+    print(table.get_string())
+
+    # print(columns_name)
+    # print(coef)
+    plt.bar(columns_name, coef)
+    plt.show()
--- main.py
+++ main.py
@@ -3,24 +3,19 @@ import pandas as pd
 import seaborn as sns
 import os
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import SGDRegressor
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.linear_model import Ridge, Lasso
-from sklearn.metrics import mean_absolute_error, mean_squared_error
+from tabulate import tabulate
+
 from sklearn.model_selection import train_test_split  # для разделение выборки
 from sklearn.preprocessing import StandardScaler  # нормировка
+from different_models import *
 
-# Регуляризация
 name = ['WorldHappiness_Corruption_2015_2020.csv',
-        'BigmacPrice.csv',
-        'googleplaystore.csv',
-        'RUvideos.csv',
-        'USvideos.csv', ]
+        'dataset.csv']
 
 file_path = os.getcwd() + '\\data\\' + name[0]
 
 
+# функции для датасета с счастьем
 def WorldHappiness_XYsplit(data):
     """Разделяем на X Y"""
     x_data = data[['Влияние ВВП',
@@ -31,14 +26,19 @@ def WorldHappiness_XYsplit(data):
                    'Влияния коррупции',]]
 
     y_data = data[['Рейтинг счастья (0-10)']]
-    print(x_data.head())
+    print(tabulate(data[['Рейтинг счастья (0-10)',
+                         'Влияние ВВП','Влияние семьи',
+                         'Влияние здоровья','Влияние свободы',
+                         'Влияние щедрости','Влияния коррупции',]]
+                   , headers='keys', tablefmt='psql'))
+
     return x_data, y_data
 
 
 def WorldHappiness_plot(data):
+    """Строим зависимости прогнозируемой переменной от параметров"""
     # print(data.columns)
     fig, ax = plt.subplots(nrows=2, ncols=3, sharey=True, figsize=(10, 10))
-
     ax[0][0].scatter(x='Влияние ВВП', y='Рейтинг счастья (0-10)', data=data)
     ax[0][0].set(xlabel='ВВП', ylabel='Счастье')
     ax[0][1].scatter(x='Влияние семьи', y='Рейтинг счастья (0-10)', data=data)
@@ -66,12 +66,14 @@ def read_WorldHappiness():
                          'generosity': 'Влияние щедрости',
                          'government_trust': 'Влияния коррупции'}, inplace=True)
 
+    data = data[data['Влияние семьи'] > 0]
     # столбцы с которыми мы будем работать
     part_dataframe = data[['Рейтинг счастья (0-10)', 'Влияние ВВП', 'Влияние семьи', 'Влияние здоровья',
                            'Влияние свободы', 'Влияние свободы', 'Влияние щедрости', 'Влияния коррупции']]
     return data, part_dataframe
 
 
+# общий участок кода для любого датасета
 def do_correlation_matrix(part_dataframe):
     """Матрицей корреляций. """
     sns.heatmap(part_dataframe.corr(),
@@ -88,110 +90,29 @@ def split_on_train_and_test(X, Y):
             }
 
 
-def print_MSE_MAE(y_train, y_test, y_train_prediction, y_test_prediction):
-    """вычисляем средневкадратичное и абсолютное ошибки"""
-    Train_MSE = mean_squared_error(y_train, y_train_prediction)
-    Test_MSE = mean_squared_error(y_test, y_test_prediction)
-    Train_MAE = mean_absolute_error(y_train, y_train_prediction)
-    Test_MAE = mean_absolute_error(y_test, y_test_prediction)
-
-    print('Train MSE: ', Train_MSE)  # чувствителен к выбросам в выборке
-    print('Test MSE: ', Test_MSE)
-
-    print('Train MAE: ', Train_MAE)  # усреднённая сумма модулей разницы между реальным и предсказанным значениями
-    print('Test MAE: ', Test_MAE)
-    return Train_MSE, Test_MSE, Train_MAE, Test_MAE
-
-
-def main():
-    print("Hello World!")
-
-
 if __name__ == "__main__":
-    result_dict = {}  # сюда запишем результаты
+
     full_data, data_part = read_WorldHappiness()
 
     WorldHappiness_plot(full_data)
 
     do_correlation_matrix(data_part)
-
+    #
     x, y = WorldHappiness_XYsplit(full_data)
-    data_dict = split_on_train_and_test(x, y)
 
+    data_dict = split_on_train_and_test(x, y)
+    #
     print(f"Записей в тренировочной выборке - {data_dict['train'][0].shape}")
     print(f"Записей в тестовой выборке - {data_dict['test'][0].shape}")
-
-    # нормировка данных
+    #
+    # # нормировка данных
     scaler = StandardScaler()
     data_dict['train'][0] = scaler.fit_transform(data_dict['train'][0])
     data_dict['test'][0] = scaler.transform(data_dict['test'][0])
 
-    # Обучим линейную регрессию и подсчитаем её качество на тесте.
-    model = LinearRegression()
-    model.fit(data_dict['train'][0], data_dict['train'][1])
+    # модели
+    weights = Linear_Regression(data_dict)
+    show_weights(x.columns, weights[0])
 
-    y_train_prediction = model.predict(data_dict['train'][0])
-    y_test_prediction = model.predict(data_dict['test'][0])
-
-    print("LinearRegression")
-    print_MSE_MAE(data_dict['train'][1], data_dict['test'][1], y_train_prediction, y_test_prediction)
-
-    # линейная регрессия с использованием градиентного спуска
-    gradient_model = SGDRegressor(tol=.0001, eta0=.01)  # прекращение итерации и скорость обучения
-    gradient_model.fit(data_dict['train'][0], data_dict['train'][1])
-
-    y_train_prediction = gradient_model.predict(data_dict['train'][0])
-    y_test_prediction = gradient_model.predict(data_dict['test'][0])
-
-    print("SGDLinearRegression")
-    print_MSE_MAE(data_dict['train'][1], data_dict['test'][1], y_train_prediction, y_test_prediction)
-
-    # # линейная регресси с полиномиальные показателями
-    # poly = PolynomialFeatures(2, include_bias=False)  # степень, исключаем x[0]**2
-    # poly_df = poly.fit_transform(data_dict['train'][0])
-    # # нормировка данных
-    # scaled_poly_df = scaler.fit_transform(poly_df)
-    # print(f" Количество показателей было: {data_dict['train'][0].shape}")
-    # print(f" Количество показателей стало: {scaled_poly_df.shape}")
-    # gradient_model.fit(scaled_poly_df, data_dict['train'][1])
-    #
-    # y_train_prediction = gradient_model.predict(data_dict['train'][0])
-    # y_test_prediction = gradient_model.predict(data_dict['test'][0])
-    # print("LinearRegression+PolynomialFeatures")
-    # print_MSE_MAE(data_dict['train'][1], data_dict['test'][1], y_train_prediction, y_test_prediction)
-
-    # Ridge Regression Model
-    ridgeReg = Ridge(alpha=10)
-
-    ridgeReg.fit(data_dict['train'][0], data_dict['train'][1])
-    y_train_prediction = ridgeReg.predict(data_dict['train'][0])
-    y_test_prediction = ridgeReg.predict(data_dict['test'][0])
-    print("Ridge")
-    print_MSE_MAE(data_dict['train'][1], data_dict['test'][1], y_train_prediction, y_test_prediction)
-
-    # Lasso Regression Model
-    lasso = Lasso(alpha=1)
-    lasso.fit(data_dict['train'][0], data_dict['train'][1])
-    y_train_prediction = lasso.predict(data_dict['train'][0])
-    y_test_prediction = lasso.predict(data_dict['test'][0])
-    print("Laso")
-    print_MSE_MAE(data_dict['train'][1], data_dict['test'][1], y_train_prediction, y_test_prediction)
-
-
-    # Визуализируем получившиеся веса
-    print(x.columns)
-    print(model.coef_[0])
-    plt.bar(x.columns, model.coef_[0])
-    plt.show()
-
-    print(gradient_model.coef_)
-    plt.bar(x.columns, gradient_model.coef_)
-    plt.show()
-
-    print(ridgeReg.coef_[0])
-    plt.bar(x.columns, ridgeReg.coef_[0])
-    plt.show()
-
-    print(lasso.coef_)
-    plt.bar(x.columns, lasso.coef_)
-    plt.show()
+    weights = Gradient_Linear_Regression(data_dict)
+    show_weights(x.columns, weights)
